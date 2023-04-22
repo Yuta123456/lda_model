5edea09deebbb22c75ec0b32e3512b230fb2bef1
Yuta123456 <yuuta09090530@icloud.com>
2023-04-22 19:06:18 +0900

    涙ぐましい努力

--- parse_expressions.py
+++ parse_expressions.py
@@ -4,19 +4,25 @@ import glob
 import MeCab
 from preProcessing.preprocessing import preprocessing
 # 'D:\M1\fashion\IQON\IQON3000'
-filepaths = glob.glob("D:/M1/fashion/IQON/IQON3000/77/**/*_new.json")
-wakati = MeCab.Tagger("-Owakati")
+# 。3966621\3966621_new.json
 
+filepaths = glob.glob("D:/M1/fashion/IQON/IQON3000/1468/**/*_new.json")
+# filepaths = glob.glob("D:/M1/fashion/IQON/IQON3000/1468/3966621/3966621_new.json")
+wakati = MeCab.Tagger("-Owakati")
 for fp in filepaths:
     try:
         json_dict = pd.read_json(fp, encoding='shift-jis')
     except Exception as e:
         # print(e)
-        # print(fp)
-        pass
+        continue
+    # print(len(json_dict["items"]), fp)
     for item in json_dict["items"]:
-        rm_br_str = preprocessing(item["expressions"][0])
-        
+        rm_br_str = preprocessing(item["expressions"][0], debug=False)
         print(rm_br_str)
+        # if (rm_br_str == ""):
+        #     print(fp)
+        #     print("="*100, "\n", item["expressions"][0], "\n","="*100)
+        # else:
+            
         # print(wakati.parse(str(rm_br_str)).split())
         
--- preProcessing/preprocessing.py
+++ preProcessing/preprocessing.py
@@ -1,16 +1,32 @@
-def preprocessing(line):
-    line = remove_bracket(line)
-    line = remove_item_description(line)
-    line = remove_sentence_after_footnote(line)
-    line = remove_after_item_number(line)
-    line = remove_after_contact_info(line)
+def preprocessing(line, debug=False):
+    preprocesser = [remove_bracket, remove_item_description,
+                     remove_sentence_after_footnote, remove_after_item_number,
+                       remove_after_contact_info, remove_after_model_info, remove_extra_info]
+    # 最後に追加
+    preprocesser.append(remove_newlines)
+    for p in preprocesser:
+        line = p(line)
+        if debug:
+            print(f"{p.__name__}",line)
     return line
 
 def remove_bracket(line):
-    index = line.find("【")  # "【" のインデックスを検索する
-    if index != -1:  # "【"が見つかった場合
-        line = line[:index]  # "【"より前の部分を取得する
-    return line
+    while True:
+        start_index = line.find("【")
+        if start_index == -1:
+            break
+        end_index_period = line.find("。", start_index)
+        end_index_newline = line.find("\r\n", start_index)
+        if end_index_period == -1:
+            end_index = end_index_newline
+        elif end_index_newline == -1:
+            end_index = end_index_period
+        else:
+            end_index = min(end_index_period, end_index_newline)
+        if end_index == -1:
+            break
+        line = line[:start_index] + line[end_index + 1:]
+    return line.strip()
 
 def remove_item_description(line):
     # アイテム説明のインデックスを検索
@@ -23,36 +39,73 @@ def remove_item_description(line):
 import re
 
 
-def remove_after_item_number(s):
-    pattern = r"(?i)\[?型番:?\s*[a-z0-9_-]+\]?"
-    match = re.search(pattern, s)
+def remove_after_item_number(line):
+    # 商品番号の削除
+    pattern = r"(?i)(商品番号|品番|型番|model)[：:\s]*[a-z0-9_-]+"
+    match = re.search(pattern, line)
     if match:
-        s = s[:match.start()]
-    pattern = r"(?i)(商品番号|型番|model)[：:]?\s*[a-z0-9_-]+"
-    match = re.search(pattern, s)
-    if match:
-        s = s[:match.start()] + s[match.end():]
-    return s.strip()
+        idx = match.start()
+        line = line[:idx] + line[match.end():]
+    return line.strip()
 
 
 
 def remove_sentence_after_footnote(line):
-    # テキスト中の「※」を検索
-    index = line.find("※")
-    if index != -1:
-        # 「※」以降の一文を検索
-        period_index = line.find("。", index)
-        if period_index != -1:
-            # 「※」以降の一文を削除
-            line = line[:index] + line[period_index+1:]
+    while True:
+        # テキスト中の「※」または「＊」を検索
+        index1 = line.find("※")
+        index2 = line.find("＊")
+        if index1 != -1 and (index2 == -1 or index1 < index2):
+            # 「※」以降の一文を検索
+            period_index = line.find("。", index1)
+            if period_index != -1:
+                # 「※」以降の一文を削除
+                line = line[:index1] + line[period_index+1:]
+            else:
+                # 「※」以降に文末がない場合は、全体を削除
+                line = line[:index1]
+        elif index2 != -1 and (index1 == -1 or index2 < index1):
+            # 「＊」以降の一文を検索
+            period_index = line.find("。", index2)
+            if period_index != -1:
+                # 「＊」以降の一文を削除
+                line = line[:index2] + line[period_index+1:]
+            else:
+                # 「＊」以降に文末がない場合は、全体を削除
+                line = line[:index2]
         else:
-            # 「※」以降に文末がない場合は、全体を削除
-            line = line[:index]
+            break
     return line
 
-def remove_after_contact_info(s):
-    pattern = r"(?i)店舗へのお問い合わせ.*"
+
+
+def remove_after_model_info(s):
+    pattern = r"(?i)モデル.*"
     match = re.search(pattern, s)
     if match:
         s = s[:match.start()]
     return s
+
+def remove_after_contact_info(s):
+    pattern1 = r"(?i)店舗へのお問い合わせ.*"
+    pattern2 = r"店舗へお問い合わせの際は下記品番をお伝えください。.*"
+    
+    match1 = re.search(pattern1, s)
+    match2 = re.search(pattern2, s)
+    
+    if match1:
+        s = s[:match1.start()]
+    elif match2:
+        s = s[:match2.start()]
+        
+    return s
+def remove_extra_info(s):
+    pattern = r"[\s\S]*(?=さらにくわしい情報をみる)"
+    match = re.match(pattern, s)
+    if match:
+        return match.group(0)
+    return s
+
+def remove_newlines(s):
+    pattern = r"\r|\n"
+    return re.sub(pattern, "", s)
