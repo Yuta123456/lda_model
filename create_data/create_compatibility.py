30a203609b89a4fbdd14840bd1646a4c7be6ca29
Yuta123456 <yuuta09090530@icloud.com>
2023-07-10 15:22:40 +0900

    互換性図るやつ

--- create_data/create_compatibility.py
+++ create_data/create_compatibility.py
@@ -4,40 +4,75 @@ KEY = "category x color"
 import pandas as pd
 import glob
 
+def is_tops(g):
+    return g in ["ジャケット", "トップス", "コート", "ニット", "タンクトップ", "ブラウス", "Tシャツ", "カーディガン", "ダウンジャケット", "パーカー"]
 
+def is_bottoms(g):
+    return g in ['スカート', 'ロングスカート', "ロングパンツ"]
+
+def is_shoes(g):
+    return g in ["ブーツ", "パンプス", "スニーカー", "靴", "サンダル"]
+
+def add_count(d, i1, i2, i3, w):
+    if i1 not in d:
+        d[i1] = {}
+    if i2 not in d[i1]:
+        d[i1][i2] = {}
+    if i3 not in d[i1][i2]:
+        d[i1][i2][i3] = 0
+    d[i1][i2][i3] += w
 # filepaths = glob.glob("D:/M1/fashion/IQON/IQON3000/**/**/*_new.json")
 fileDir = glob.glob("D:/M1/fashion/IQON/IQON3000/**/")
 filepaths = []
 for fd in fileDir:
     filepaths += glob.glob(fd + "**/*_new.json")
+tops_count = {}
+bottoms_count = {}
+shoes_count = {}
 
-count = {}
 file_count = len(filepaths)
 print(len(filepaths))
-c = 0
-for fp in filepaths:
-    c += 1
+for c, fp in enumerate(filepaths):
     try:
-        json_dict = pd.read_json(fp, encoding='shift-jis')
+        coordinate = pd.read_json(fp, encoding='shift-jis')
     except Exception as e:
         continue
-    for item in json_dict["items"]:
+    # 各コーディネートの情報が手に入る
+    # 一応、like_countもある。
+    weight = coordinate["likeCount"]
+    weight = 1 if len(weight) == 0 else int(weight[0])
+    tops = []
+    bottoms = []
+    shoes = []
+    for item in coordinate["items"]:
         if (KEY not in item or len(item[KEY]) == 0):
             continue
         category = item[KEY]
         garment = category.split(' × ')[0]
-        if garment == "":
-            continue
-        if garment in count:
-            count[garment] += 1
-        else:
-            count[garment] = 1
+        if is_tops(garment):
+            tops.append(category)
+        if is_bottoms(garment):
+            bottoms.append(category)
+        if is_shoes(garment):
+            shoes.append(category)
+    for t in tops:
+        for b in bottoms:
+            for s in shoes:
+                add_count(tops_count, b, s, t, weight)
+                add_count(bottoms_count, t, s, b, weight)
+                add_count(shoes_count, t, b, s, weight)
     if (c % 100 == 0):
-        print(f"{c * 100 / file_count}%終了")
-import json
-with open('./category.json', 'w', encoding='shift-jis') as f:
-    json.dump(count, f, ensure_ascii=False)
-count = sorted(count.items(), key=lambda x: x[1], reverse=True)
-save_word_histogram(count, 'category.png')
+        p = (c * 100 // file_count)
+        progress = '=' * p + ' ' * (100-p)
+        print(f"\r【{progress}】{p}%", end='')
 
+import json
+with open('./tops_count.json', 'w', encoding='shift-jis') as f:
+    json.dump(tops_count, f, ensure_ascii=False)
+
+with open('./bottoms_count.json', 'w', encoding='shift-jis') as f:
+    json.dump(bottoms_count, f, ensure_ascii=False)
+
+with open('./shoes_count.json', 'w', encoding='shift-jis') as f:
+    json.dump(shoes_count, f, ensure_ascii=False)
 
